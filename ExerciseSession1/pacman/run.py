854c75c7038c73e5b72f11f3abeef0abfdf5dcd1
lukyrasocha <rasocha.lukas@gmail.com>
2023-02-06 15:38:20 +0100

    Add main script to run pacman

---
+++ ExerciseSession1/pacman/run.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.locals import *
+from constants import *
+
+class GameController(object):
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
+        self.background = None
+        self.clock = pygame.time.Clock()
+
+
+    def setBackground(self):
+        self.background = pygame.surface.Surface(SCREENSIZE).convert()
+        self.background.fill(BLACK)
+
+    def startGame(self):
+        self.setBackground()
+
+    def update(self):
+        self.checkEvents()
+        self.render()
+
+    def checkEvents(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+
+    def render(self):
+        pygame.display.update()
+
+if __name__ == "__main__":
+    game = GameController()
+    game.startGame()
+    while True:
+        game.update()
