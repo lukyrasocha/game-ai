ea8c02090be77f91be24ab091aebcb6df81499c9
Lukas Rasocha <rasocha.lukas@gmail.com>
2023-02-11 15:08:08 +0100

    Move common code to its superclass, implement flee behaviour instead of manual controlling

--- ExerciseSession1/pacman/pacman.py
+++ ExerciseSession1/pacman/pacman.py
@@ -7,42 +7,28 @@ from entity import Entity
 
 class Pacman(Entity):
     def __init__(self, node):
-        Entity.__init__(self, node )
+        Entity.__init__(self, node)
         self.name = PACMAN
         self.color = YELLOW
+        self.directionMethod = self.fleeDirection
 
+    #def update(self, dt):	
+    #    self.position += self.directions[self.direction]*self.speed*dt
+    #    direction = self.getValidKey()
+    #    if self.overshotTarget():
+    #            self.node = self.target
+    #            self.target = self.getNewTarget(direction)
+    #            if self.target is not self.node:
+    #                self.direction = direction
+    #            else: # so then if you have o----x-----x the pacman would continue to the right most x without stopping at the first x
+    #                self.target = self.getNewTarget(self.direction)
 
-    def setPosition(self):
-        self.position = self.node.position.copy()
-
-    def update(self, dt):	
-        self.position += self.directions[self.direction]*self.speed*dt
-        direction = self.getValidKey()
-        if self.overshotTarget():
-                self.node = self.target
-                self.target = self.getNewTarget(direction)
-                if self.target is not self.node:
-                    self.direction = direction
-                else: # so then if you have o----x-----x the pacman would continue to the right most x without stopping at the first x
-                    self.target = self.getNewTarget(self.direction)
-
-                if self.target is self.node:
-                    self.direction = STOP
-                self.setPosition()
-        else: 
-            if self.oppositeDirection(direction):
-                self.reverseDirection()
-
-    def validDirection(self, direction):
-        if direction is not STOP:
-            if self.node.neighbors[direction] is not None:
-                return True
-        return False
-
-    def getNewTarget(self, direction):
-        if self.validDirection(direction):
-            return self.node.neighbors[direction]
-        return self.node
+    #            if self.target is self.node:
+    #                self.direction = STOP
+    #            self.setPosition()
+    #    else: 
+    #        if self.oppositeDirection(direction):
+    #            self.reverseDirection()
 
     def getValidKey(self):
         key_pressed = pygame.key.get_pressed()
@@ -56,28 +42,6 @@ class Pacman(Entity):
             return RIGHT
         return STOP
 
-    def overshotTarget(self):
-        if self.target is not None:
-            vec1 = self.target.position - self.node.position
-            vec2 = self.position - self.node.position
-            node2Target = vec1.magnitudeSquared()
-            node2Self = vec2.magnitudeSquared()
-            return node2Self >= node2Target
-        return False
 
-    def reverseDirection(self):
-        self.direction *= -1
-        temp = self.node
-        self.node = self.target
-        self.target = temp
 
-    def oppositeDirection(self, direction):
-        if direction is not STOP:
-            if direction == self.direction * -1:
-                return True
-        return False 
-
-    def render(self, screen):
-        p = self.position.asInt()
-        pygame.draw.circle(screen, self.color, p, self.radius)
 
